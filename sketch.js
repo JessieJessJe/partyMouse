29cac4fb9546ad70ddac0a79a9ee48f33c56845f
JessieJessJe <jiahuihan@yahoo.com>
2022-03-27 16:38:51 -0400

    connect

--- sketch.js
+++ sketch.js
@@ -262,7 +262,11 @@ function draw() {
     //my.dir = 'none';
   }
   
-  
+  /////////////front end 
+ if (!keyIsDown(39) && !keyIsDown(37) && !keyIsDown(38) && !keyIsDown(40)){
+    handleParentEvent();
+ }
+
   /////////////////////////////////////
   //will update positions later
   // my.body.x += my.dX;
@@ -517,4 +521,43 @@ function draw() {
   push();
   
   my.dir = 'none';
+}
+
+
+///////////front end 
+let parentKeyCode = 0; 
+
+window.addEventListener("message", (e)=>{
+    console.log(e.data, 'receive')
+    parentKeyCode = e.data;
+}) 
+
+function handleParentEvent(){
+
+  if(parentKeyCode === 38 && my.body.y > 10){
+    my.dY = -3;
+    my.dir = 'top';
+  }else if(parentKeyCode === 40 && my.body.y < height - 10){
+    my.dY = 3;
+    my.dir = 'bottom';
+  }else{
+    my.dY = 0;
+    //my.dir = 'none';
+  }
+
+  if(parentKeyCode === 37 && my.body.x > 10){
+    my.dX = -3;
+    my.dir = 'left';
+    my.facing = 'left';
+  }else if( parentKeyCode === 39 && my.body.x < width - 10){
+    my.dX = 3;
+    my.dir = 'right';
+    my.facing = 'right';
+  }else{
+    my.dX = 0;
+    //my.dir = 'none';
+  }
+  
+  
+
 }
